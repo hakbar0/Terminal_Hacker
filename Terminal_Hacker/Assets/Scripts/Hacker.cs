56c36b3221c410fd12d298e9d6809e2d04744e56
Haseeb Akbar <haseeb2000@hotmail.com>
2018-05-26 18:13:47 +0100

    Added text to the terminal.

---
+++ Terminal_Hacker/Assets/Scripts/Hacker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Terminal.WriteLine("What would you like to hack into?");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Press 1 for local takeaway.");
+        Terminal.WriteLine("Press 2 for local hospital.");
+        Terminal.WriteLine("Press 3 for psn servers.");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Enter you selection.");
+        Terminal.WriteLine("");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
